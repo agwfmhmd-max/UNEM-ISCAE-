08c90e36f9d4b0f59ea50f882350aadd70a57fe1
Mohamed Dah Agove <agwfmhmd@gmail.com>
2026-09-18 22:07:27 +0000

    Update CACHE_NAME to version 23

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 // UNEM ISCAE Service Worker
 // ملاحظة: التطبيق يقرأ رقم الإصدار من CACHE_NAME لعرض رسالة التحديث المناسبة.
-const CACHE_NAME = 'unem-iscae-v22';
+const CACHE_NAME = 'unem-iscae-v23';
 const PRECACHE = ['/', '/manifest.json', '/icon-192.png'];
 
 self.addEventListener('install', (event) => {
